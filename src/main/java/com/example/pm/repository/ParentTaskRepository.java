a16236b03cf75fd9127119f6de353560bf97cb27
jubin <jubin@gmail.com>
2017-12-26 20:10:22 +0530

    first commit

---
+++ src/main/java/com/example/pm/repository/ParentTaskRepository.java
@@ -0,0 +1,9 @@
+package com.example.pm.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.pm.dao.Parent_Task;
+
+public interface ParentTaskRepository extends CrudRepository<Parent_Task, Long> {
+
+}
---
+++ src/main/java/com/example/pm/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.example.pm.repository;
+
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.pm.dao.User;
+
+
+// This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
+// CRUD refers Create, Read, Update, Delete
+
+public interface UserRepository extends CrudRepository<User, Long> {
+
+}
